a38b75edbd95040a4e2e9d9f38ca5559bf1e2ec5
Emmanuel Marchal <81331382+irbt@users.noreply.github.com>
2022-12-08 14:55:08 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// Write a C# Sharp program to calculate the average word length in a given string. Round the average length up to two decimal places.
+using System.Text.RegularExpressions;
+namespace Program{
+    class AverageLength{
+        public static void Main (string[] args){
+            
+        }
+    }
+}
